6348139d8f6f0f3d2ca59c8a00291f535365b44b
Gubner <90917279+Gubner@users.noreply.github.com>
2022-06-19 08:50:49 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 Config = {}
 
 Config.UpdateFrequency = 5000 -- in milliseconds
-Config.DisplayX = 0.9 -- % viewport width from left
-Config.DisplayY = 96.3 -- % viewport height from top
+Config.DisplayX = 0.9 -- % viewport offset from left
+Config.DisplayY = 96.3 -- % viewport offset from top
 Config.TemperatureTextDisplay = true -- false for numeric values
 Config.TemperatureText = {"Frigid", "Cold", "Pleasant", "Warm", "Hot"} -- five ranges from coldest to hottest
 Config.Units = "F" -- F or C
-Config.Shadow = true
+Config.Shadow = true -- Add drop shadow to text
 Config.UseBackgroundImage = true -- if true, uses background.png and ignores Background and BackgroundColor
-Config.Background = false
+Config.Background = false -- if true, use a solid background color
 Config.BackgroundColor = "black" -- css color name
